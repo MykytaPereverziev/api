bfba4a45ec9f8c9e58b9dc749e31e09ea59522ae
Nikita Pereverziev <silvernik9@gmail.com>
2019-09-18 18:30:17 +0300

    reg

--- public/main2.js
+++ public/main2.js
@@ -1,4 +1,3 @@
-
 /*axios.post('/users', {name: 'Kostian', surname: 'Gykov'}).then(res => {
     console.log(res);
 }).catch(err => console.log(err));
@@ -11,7 +10,8 @@ xhr.send();
 */
 
 //let promiseObj = new Promise((reselve, reject) => {
-    const fetchApi = url => {
+/*Do in class    
+const fetchApi = url => {
     return new Promise((reselve, reject) => {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -29,7 +29,8 @@ xhr.send();
     fetchApi('/users').then(reselve => {
         console.log(reselve)
     });
-     
+    */
+
     /*
     promiseObj.then(reselve =>{
         console.log(reselve)
@@ -49,3 +50,48 @@ console.log('end', new Date().getSeconds());
 */
 //console.log('end', new Date().getSeconds());
 
+   //Do at home:
+
+class Fetch(){
+    constructor(){
+
+    };
+    get(url){
+        return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = () => { 
+            const response = JSON.parse(xhr.responseText);
+        resolve(response);
+        }
+        xhr.onerror = () => {
+            reject(JSON.parse(xhr.responseText));
+        }
+        xhr.send()
+    });
+};
+    post(url, body){
+        return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.onload = () => { 
+            resolve(JSON.parse(xhr.responseText));
+        }
+        xhr.onerror = () => {
+            reject(JSON.parse(xhr.responseText));
+        }
+        xhr.send(JSON.stringify(body))
+    });
+
+    getByParams(url, parametr){
+        return this.get(`${url}/${param}`)
+    }
+};
+
+let fetch = new Fetch();
+fetch.get('/users').then(res => console.log(res));
+
+fetch.post('/users', {name: 'Jenny', surname: 'Smith'}).then(res => console.log(res));
+
+fetch.getByParams('/users', 'Smith').then(res => console.log(res));
